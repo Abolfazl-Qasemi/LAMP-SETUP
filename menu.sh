cb7940735f1be406b0deed5bc48ce8d59cd86ce0
Abolfazl <abolfazl.77.q86@gmail.com>
2026-01-03 12:41:14 +0330

    WIP on hotfix/paths
    pop from anothe device

--- menu.sh
+++ menu.sh
@@ -2,7 +2,7 @@
 
 cd /home/$USER/Desktop
 
-source LAMP-setup/variable.sh
+source LAMP-SETUP/variable.sh
 
 echo "Welcome to LAMP-setup!"
 echo -e "\e[36mFor install and test ---------> 1 \n\nFor remove -------------------> 2 \n\nTo test the installed or not -> 3\e[0m \n\n\n\n"
@@ -10,13 +10,13 @@ read -p "Select number and press enter: " menuInput
 
 case "$menuInput" in
     1)
-    source LAMP-setup/install-lamp.sh
-    source LAMP-setup/test-lamp.sh
+    source LAMP-SETUP/install-lamp.sh
+    source LAMP-SETUP/test-lamp.sh
     ;;
     2) 
-    source LAMP-setup/remove-lamp.sh
+    source LAMP-SETUP/remove-lamp.sh
     ;;
     3) 
-    source LAMP-setup/test-lamp.sh
+    source LAMP-SETUP/test-lamp.sh
     ;;
 esac
