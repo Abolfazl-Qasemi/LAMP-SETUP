e292a4043f2c686094caff9e095645316c28eee8
Abolfazl <abolfazl.77.q86@gmail.com>
2026-01-03 12:57:08 +0330

    fix: "apt update" and creat folder of info.php

--- install-lamp.sh
+++ install-lamp.sh
@@ -8,24 +8,25 @@ echo "This program install Apache , MariaDB-server and PHP, along with all confi
 read -p "Do you want to continue? [Y/n] " warningAllert
 if [[ "$warningAllert" == "Y" ]]; then
     sleep 4
-    echo "Start process..."
-    sudo apt update 
-    
-    for package in "${allPackages[@]}"; do
-        echo "----------------------------------------------------"
-        #package installed or not
-        if [[ "$(dpkg -s $package | grep "Status" | cut -d ':' -f2)" != " install ok installed" ]]; then 
-            sudo apt install $package -y
-        fi
+    if sudo apt update; then      
+        echo "Start process..."
+        for package in "${allPackages[@]}"; do
+            echo "----------------------------------------------------"
+            #package installed or not
+            if [[ "$(dpkg -s $package | grep "Status" | cut -d ':' -f2)" != " install ok installed" ]]; then 
+                sudo apt install $package -y
+            fi
 
-    done 
-    #creat info.php file
-    if [[ ! -e "/var/www/html/info.php" ]]; then
-        #print code -> than write to info.php -> at the end paste to null for clean terminal 
-        echo -e "<?php \nphpinfo(); \n?>" | sudo tee /var/www/html/info.php > /dev/null 
-        
-    fi
-    sudo systemctl restart apache2
+        done 
+        #creat info.php file
+        if [[ ! -e "/var/www/html/info.php" ]]; then
+            sudo mkdir -p /var/www/html/
+            #print code -> than write to info.php -> at the end paste to null for clean terminal 
+            echo -e "<?php \nphpinfo(); \n?>" | sudo tee /var/www/html/info.php > /dev/null 
+
+        fi
+        sudo systemctl restart apache2
+        fi
 else
     echo "Bye"
 fi
